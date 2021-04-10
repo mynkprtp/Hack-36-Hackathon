564ffb56eb97d5324efa501dcd5f982e3be86309
Mayank Pratap Singh <itsmayankhere24@gmail.com>
2021-04-10 16:00:28 +0530

    style.css add to all

--- pages/history.php
+++ pages/history.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>History</title>
         <link rel="icon" href="https://www.flaticon.com/premium-icon/icons/svg/2853/2853896.svg" type="image/x-icon">
+        <link rel="stylesheet" href="../styles/style.css">
         <link rel="stylesheet" href="../styles/history.css">
         
     </head>
